27ac257201b59f9c21b427303fe8d9944fb0c122
Dean Schmigelski <dbschmigelski+github@gmail.com>
2025-06-05 16:15:16 +0300

    testing integ on mcp

---
+++ tests-integ/test_mcp_stdio.py
@@ -0,0 +1,58 @@
+import threading
+from typing import List, Literal
+
+from mcp import StdioServerParameters, stdio_client
+
+from strands import Agent
+from strands.tools.mcp.mcp_client import MCPClient
+from strands.tools.mcp.mcp_types import MCPTransport
+from strands.types.content import Message
+from strands.types.tools import ToolUse
+
+
+def start_calculator_server(transport: Literal["sse", "streamable-http"], port=int):
+    """
+    Initialize and start an MCP calculator server for integration testing.
+
+    This function creates a FastMCP server instance that provides a simple
+    calculator tool for performing addition operations. The server uses
+    Server-Sent Events (SSE) transport for communication, making it accessible
+    over HTTP.
+    """
+    from mcp.server import FastMCP
+
+    mcp = FastMCP("Calculator Server", port=port)
+
+    @mcp.tool(description="Calculator tool which performs calculations")
+    def calculator(x: int, y: int) -> int:
+        return x + y
+
+    @mcp.tool(description="Generates a custom image")
+    def generate_custom_image() -> MCPImageContent:
+        try:
+            with open("tests-integ/test_image.png", "rb") as image_file:
+                encoded_image = base64.b64encode(image_file.read())
+                return MCPImageContent(type="image", data=encoded_image, mimeType="image/png")
+        except Exception as e:
+            print("Error while generating custom image: {}".format(e))
+
+    mcp.run(transport=transport)
+
+
+def test_mcp_client():
+    """
+    Test should yield output similar to the following
+    {'role': 'user', 'content': [{'text': 'add 1 and 2, then echo the result back to me'}]}
+    {'role': 'assistant', 'content': [{'text': "I'll help you add 1 and 2 and then echo the result back to you.\n\nFirst, I'll calculate 1 + 2:"}, {'toolUse': {'toolUseId': 'tooluse_17ptaKUxQB20ySZxwgiI_w', 'name': 'calculator', 'input': {'x': 1, 'y': 2}}}]}
+    {'role': 'user', 'content': [{'toolResult': {'status': 'success', 'toolUseId': 'tooluse_17ptaKUxQB20ySZxwgiI_w', 'content': [{'text': '3'}]}}]}
+    {'role': 'assistant', 'content': [{'text': "\n\nNow I'll echo the result back to you:"}, {'toolUse': {'toolUseId': 'tooluse_GlOc5SN8TE6ti8jVZJMBOg', 'name': 'echo', 'input': {'to_echo': '3'}}}]}
+    {'role': 'user', 'content': [{'toolResult': {'status': 'success', 'toolUseId': 'tooluse_GlOc5SN8TE6ti8jVZJMBOg', 'content': [{'text': '3'}]}}]}
+    {'role': 'assistant', 'content': [{'text': '\n\nThe result of adding 1 and 2 is 3.'}]}
+    """  # noqa: E501
+
+    stdio_mcp_client = MCPClient(
+        lambda: stdio_client(StdioServerParameters(command="python", args=["tests-integ/echo_server.py"]))
+    )
+    with stdio_mcp_client:
+        agent = Agent(tools=stdio_mcp_client.list_tools_sync())
+    print("DONE")
